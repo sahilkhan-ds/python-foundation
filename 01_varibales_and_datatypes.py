28f87ec49c9bcde9aa03ee55436b71d91deb33ff
sahilkhan-ds <sahilkhan88508@gmail.com>
2026-02-26 09:23:38 +0530

    Create 01_varibales_and_datatypes.py
    Added variables and datatypes practice

---
+++ 01_varibales_and_datatypes.py
@@ -0,0 +1,10 @@
+Python
+age = 25 #int
+height = 5.8 #float
+name = 'Sahil' #str
+is_learning = True #bool
+
+print('Age type:',type(age))
+print('Height type:',type(height))
+print('Name type:',type(name))
+print('Learning status type:',type(is_learning))
