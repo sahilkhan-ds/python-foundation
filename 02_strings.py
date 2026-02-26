990d8509e3309e47a65a078c9d8bb2af594f7b6f
sahilkhan-ds <sahilkhan88508@gmail.com>
2026-02-26 09:28:09 +0530

    Create 02_strings.py
    Added string practice

---
+++ 02_strings.py
@@ -0,0 +1,11 @@
+Python
+
+#String basics
+name = "Sahil Khan"
+
+print("Original",name)
+print("Uppercase",name.upper())
+print("Lowercase",name.lower())
+print("Length",len(name))
+print("First character",name[0])
+print("Last character",name[-1])
